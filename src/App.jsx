f5ec963c3563a82eeb0cc2fad9a0914b19ff3b02
Quan L. <45319129+Snowflaker1080@users.noreply.github.com>
2025-08-08 04:05:47 +1000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useEffect, useState} from "react";
+import * as petService from "./services/petServices";
 import "./App.css";
 
-const App = () => {
+function App () {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const getData =async () => {
+    try {
+    const fetchedPets = await petService.index();
+    // Call on the pet service's index function
+    setPets(fetchedPets);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  getData ();
+},[]);
+
+console.log("Pets are: ", pets);
+
   return <h1>Hello world!</h1>;
 };
 
